1a27dfe794e605a13a43b7432714d64203900375
asikaim <markku@tangible-growth.com>
2020-03-05 17:24:57 +0200

    quit app & run

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 # main window of the notepad
 import os
-from tkinter import Tk
+from tkinter import *
 
 
 class Notepad:
+    __root = Tk()
+    __thisWidth = 360
+    __thisHeight = 640
+
     def __init__(self, **kwargs):
         pass
+
+    def __quitApplication(self):
+        self.__root.destroy()
+        # exit()
+
+    def run(self):
+        # Run main application
+        self.__root.mainloop()
 
 
 if __name__ == '__main__':
     notepad = Notepad(width=360, height=640)
+    notepad.run()
